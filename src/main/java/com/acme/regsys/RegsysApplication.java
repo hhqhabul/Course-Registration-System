c756db74dd011d5c3e9e5221feca64e2c183b0ec
Hassan Abul <hhqh.abul@gmail.com>
2025-08-21 20:17:29 -0400

    the start

---
+++ src/main/java/com/acme/regsys/RegsysApplication.java
@@ -0,0 +1,13 @@
+package com.acme.regsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/regsys/RegsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.regsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
